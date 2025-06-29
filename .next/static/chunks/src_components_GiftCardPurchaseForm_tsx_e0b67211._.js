dd57f8fff7df1270c5594721b5258901c426678c
Egremont09 <enquiries@sofiagraceadvertising.co.uk>
2025-06-29 06:00:09 +0100

    Add .next/static/chunks/src_components_GiftCardPurchaseForm_tsx_e0b67211._.js 1751173209652

---
+++ .next/static/chunks/src_components_GiftCardPurchaseForm_tsx_e0b67211._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/GiftCardPurchaseForm.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/_3b9a486d._.js",
+  "static/chunks/src_components_GiftCardPurchaseForm_tsx_5e94db78._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/GiftCardPurchaseForm.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
